0d9f9f6b8cdc5578d9e4e51886b4faf2f68374a3
Matthew Li <matthewli2491@gmail.com>
2019-11-09 16:36:56 -0500

    Created new xCode project and added assets

---
+++ Parstagram/Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Super MattMatt on 11/9/19.
+//  Copyright © 2019 Parstagram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
